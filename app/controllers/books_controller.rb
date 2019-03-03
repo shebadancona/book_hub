551698159badaf51899aa9234fbf3599c53f9b7e
shebadancona <shebadancona@gmail.com>
2019-03-03 18:25:28 -0500

    book application

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+      @book = Book.new(title: book_params[:title], description: book_params[:description], isbn: book_params[:isbn])
+      if @book.save 
+       redirect_to @book
+      else
+        render 'new'
+      end
+  end
+
+  def show
+   @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private 
+  def book_params
+    params.require(:book).permit(:title, :description, :isbn)
+  end
+
+end
